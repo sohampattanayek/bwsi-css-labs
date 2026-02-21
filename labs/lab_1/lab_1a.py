a560215b8e4bb85825c7ed2db188f258cdc481d9
sohampattanayek <sohampattanayek1234@gmail.com>
2026-02-20 20:29:26 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Soham Pattanayek" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey! My name is Soham Pattanayek, and I am a Sophomore from Florida. I am interested in engineering and computer science. I love designing things using CADs and using microelectronics to create projects. I also love to code and learn about AI Machine Learning. Aside from STEM, I enjoy doing business and marketing projects in my school's DECA and boxing at my local UFC gym. Recently, I have been particularly interested in Cloud Computing, and have been working on doing my AWS Cloud Computing Certification. I can't wait to learn more with all of you!")
 
 if __name__ == "__main__":
     main()
